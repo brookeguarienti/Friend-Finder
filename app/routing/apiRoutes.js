87ae3ae108d53f19135e5ef0c918d904336cd046
Brooke G <brooke.a.guarienti@gmail.com>
2020-04-30 13:04:56 -0500

    updated apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,14 @@
 var friendsData = require("../data/friends");
 
 module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(tableData);
+      });
 
+      app.post("/api/friends", function(req, res) {
+        friendsArray.length = 0;
+    
+        res.json({ ok: true });
+
+      });
 }
